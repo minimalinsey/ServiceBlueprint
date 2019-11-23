415476f64dc6c13b65ad5c9e608d7cf917aaa3d6
minimalinsey <48240294+minimalinsey@users.noreply.github.com>
2019-11-23 10:59:18 +0100

    Content v1
    Content er op gezet voor de feel. Layout van Notes gepakt voor home page.

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,9 +2,9 @@
 /**
  * Templates render the content of your pages. 
  * They contain the markup together with some control structures like loops or if-statements.
+ * This template is responsible for rendering all the subpages of the `notes` page.
  * The `$page` variable always refers to the currently active page. 
- * To fetch the content from each field we call the field name as a method on the `$page` object, e.g. `$page->title()`. 
- * This home template renders content from others pages, the children of the `photography` page to display a nice gallery grid.
+ * To fetch the content from each field we call the field name as a method on the `$page` object, e.g. `$page->title()`.
  * Snippets like the header and footer contain markup used in multiple templates. They also help to keep templates clean.
  * More about templates: https://getkirby.com/docs/guide/templates/basics
  */
@@ -13,36 +13,19 @@
 <?php snippet('header') ?>
 
 <main>
-  <?php snippet('intro') ?>
+  <article class="note">
+    <header class="note-header intro">
+      <h1><?= $page->title() ?></h1>
+      <time class="note-date"><?= $page->date()->toDate('d F Y') ?></time>
+      <?php if ($page->tags()->isNotEmpty()) : ?>
+      <p class="note-tags tags"><?= $page->tags() ?></p>
+      <?php endif ?>
+    </header>
 
-
-  <?php 
-  // we always use an if-statement to check if a page exists to prevent errors 
-  // in case the page was deleted or renamed before we call a method like `children()` in this case
-  if ($photographyPage = page('photography')): ?>
-  <ul class="grid">
-    <?php foreach ($photographyPage->children()->listed() as $album): ?>
-    <li>
-      <a href="<?= $album->url() ?>">
-        <figure>
-          <?php 
-          // the `cover()` method defined in the `album.php` page model can be used 
-          // everywhere across the site for this type of page
-          if ($cover = $album->cover()): ?>
-          <?= $cover->resize(1024, 1024) ?>
-          <?php endif ?>
-          <figcaption>
-            <span>
-              <span class="example-name"><?= $album->title() ?></span>
-            </span>
-          </figcaption>
-        </figure>
-      </a>
-    </li>
-    <?php endforeach ?>
-  </ul>
-  <?php endif ?>
-
+    <div class="note-text text">
+      <?= $page->text()->kt() ?>
+    </div>
+  </article>
 </main>
 
 <?php snippet('footer') ?>
